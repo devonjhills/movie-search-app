211b9d1e7ae1904e4559ca4b6ce9d9286d758b0b
devonjhills <devonjhills@gmail.com>
2021-09-05 00:53:41 -0400

    netlify refresh fix test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import MenuNavbar from "./components/MenuNavbar";
 import MovieHub from "./components/MovieHub";
 import TvHub from "./components/TvHub";
